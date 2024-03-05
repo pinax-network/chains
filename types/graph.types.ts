2662690342715141b494fffaa58125dc385c5ec0
Dominic Fournier <me@dominicfournier.com>
2024-03-04 19:33:06 -0500

    Fixed chains export

--- types/graph.types.ts
+++ types/graph.types.ts
@@ -1,6 +1,6 @@
 // This file is auto-generated on pre-commit to avoid maintaining it.
 // Do not modify manually as it will be overwritten.
-// Last generation on 2024-03-04, 6:41:00 p.m..
+// Last generation on 2024-03-04, 7:11:37 p.m..
 export type GraphId =
   | 'arbitrum-goerli'
   | 'arbitrum-one'
