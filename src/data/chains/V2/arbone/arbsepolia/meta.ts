a06da8a6a5ea667f069cf8fa75e10230c2daebb4
Dominic Fournier <me@dominicfournier.com>
2024-05-08 16:18:44 -0400

    Added missing Arbsepolia

--- src/data/chains/V2/arbone/arbsepolia/meta.ts
+++ src/data/chains/V2/arbone/arbsepolia/meta.ts
@@ -7,15 +7,15 @@ const meta: Testnet = {
   alt_names: [],
   supported_services: {
     firehose: {
-      released_at: null,
+      released_at: '2024-05-03T00:00:00.000Z',
       deprecated_at: null,
     },
     substreams: {
-      released_at: null,
+      released_at: '2024-05-03T00:00:00.000Z',
       deprecated_at: null,
     },
     rpc: {
-      released_at: null,
+      released_at: '2024-05-03T00:00:00.000Z',
       deprecated_at: null,
     },
   },
--- src/types/pinax.types.ts
+++ src/types/pinax.types.ts
@@ -1,6 +1,6 @@
 // This file is auto-generated on pre-commit to avoid maintaining it / circular dependencies.
 // Do not modify manually as it will be overwritten.
-// Last generation on 5/8/2024, 3:14:32 PM.
+// Last generation on 5/8/2024, 4:18:45 PM.
 export type PinaxID =
   | 'arbgoerli'
   | 'arbsepolia'
