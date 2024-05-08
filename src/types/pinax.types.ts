e842e0ebcfef6d4a623ab3a7266ecf0fdaf0518c
Dominic Fournier <me@dominicfournier.com>
2024-05-08 15:14:31 -0400

    Updated RC version

--- src/types/pinax.types.ts
+++ src/types/pinax.types.ts
@@ -1,6 +1,6 @@
 // This file is auto-generated on pre-commit to avoid maintaining it / circular dependencies.
 // Do not modify manually as it will be overwritten.
-// Last generation on 5/8/2024, 3:13:35 PM.
+// Last generation on 5/8/2024, 3:14:32 PM.
 export type PinaxID =
   | 'arbgoerli'
   | 'arbsepolia'
