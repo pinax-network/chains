952e73f145d6b2ce4d70a850531d4254e8f63167
Dominic Fournier <me@dominicfournier.com>
2024-08-07 16:47:10 -0400

    Split ChainIcon type for reusability, added missing utility functions from website.

--- test.ts
+++
@@ -1,5 +0,0 @@
-import * as chains from './build/index'
-
-console.log(chains.chains.arbone.branded)
-console.log(chains.chains.arweave.branded)
-console.log(chains.chains.arweave.meta)
--- utils/chains.ts
+++ utils/chains.ts
@@ -174,6 +174,34 @@ const findChainById = (
 };
 
 /**
+ * Finds the mainnet chain or its subnet by ID from the given database.
+ *
+ * @param {Array<Chain>} db - The database of chains to search through.
+ * @param {string} id - The ID of the chain or subnet to find.
+ * @returns {Chain | undefined} - The chain containing the subnet with the given ID, or undefined if not found.
+ */
+const findSubnetMainnet = (db: Array<Chain>, id: string) => {
+  for (const chain of db) {
+    if (chain.id === id) {
+      return chain;
+    }
+    const consensus = chain.consensus?.find((consensus) => consensus.id === id);
+    if (consensus) {
+      return chain;
+    }
+    const testnet = chain.testnets?.find((testnet) => testnet.id === id);
+    if (testnet) {
+      return chain;
+    }
+    const evm = chain.evms?.find((evm) => evm.id === id);
+    if (evm) {
+      return chain;
+    }
+  }
+  return undefined;
+};
+
+/**
  * Checks whether a chain has full block support. Chains that use RPC poller only
  * support partial blocks.
  *
@@ -185,6 +213,100 @@ const hasChainFullBlockSupport = (chain: Chain | Testnet | ConsensusLayer) => {
   return chain.is_detailed_blocks;
 };
 
+/**
+ * Calculates the number of supported chains from a given array of chains.
+ *
+ * @param {Array<Chain | Testnet | ConsensusLayer | EVM>} chains - The array of chains to check for support.
+ * @returns {number} The number of supported chains.
+ */
+const getNumberOfSupportedChains = (
+  chains: Array<Chain | Testnet | ConsensusLayer | EVM>,
+) => {
+  let supportedChains = 0;
+  chains
+    .filter((c: any) => isChainSupported(c))
+    .forEach((c: any) => {
+      supportedChains++;
+      c.consensus?.forEach((consensus: any) => {
+        if (isChainSupported(consensus)) {
+          supportedChains++;
+        }
+      });
+      c.evms?.forEach((evm: any) => {
+        if (isChainSupported(evm)) {
+          supportedChains++;
+        }
+      });
+      c.testnets?.forEach((tn: any) => {
+        if (isChainSupported(tn)) {
+          supportedChains++;
+        }
+      });
+    });
+  return supportedChains;
+};
+
+/**
+ * Determines the status of a given chain.
+ *
+ * @param {Chain | ConsensusLayer | EVM | Testnet} chain - The chain to check the status of.
+ * @returns {string} The status of the chain, which can be "supported", "beta", "deprecated", or "unsupported".
+ */
+const getChainStatus = (chain: Chain | ConsensusLayer | EVM | Testnet) => {
+  if (isChainSupported(chain)) {
+    return 'supported';
+  }
+  if (isChainBeta(chain)) {
+    return 'beta';
+  }
+  if (isChainDeprecated(chain)) {
+    return 'deprecated';
+  }
+  return 'unsupported';
+};
+
+/**
+ * Retrieves the supported services for a given chain.
+ *
+ * @param {Chain | Testnet | ConsensusLayer | EVM} chain - The chain object to check for supported services.
+ * @returns {Array<[ServiceID, string | null]>} An array of tuples where each tuple contains a service ID and the release date (beta or full) of the service.
+ */
+const getSupportedServices = (
+  chain: Chain | Testnet | ConsensusLayer | EVM,
+) => {
+  let supServices = [] as Array<[ServiceID, string | null]>;
+  (['rpc', 'firehose', 'substreams'] as Array<ServiceID>).forEach((service) => {
+    if (isServiceBeta(chain as any, service as any)) {
+      supServices.push([
+        service,
+        chain.supported_services[
+          service as keyof typeof chain.supported_services
+        ]?.beta_released_at,
+      ]);
+    } else if (isServiceSupported(chain as any, service as any)) {
+      supServices.push([
+        service,
+        chain.supported_services[
+          service as keyof typeof chain.supported_services
+        ]?.full_released_at,
+      ]);
+    }
+  });
+  return supServices;
+};
+
+/**
+ * Retrieves the subnets of a given chain by concatenating its testnets, evms, and consensus arrays.
+ *
+ * @param {Chain} chain - The chain object containing testnets, evms, and consensus arrays.
+ * @returns {Array<any>} - An array containing all subnets from the testnets, evms, and consensus arrays.
+ */
+const getChainSubnets = (chain: Chain) => {
+  return (chain.testnets as Array<any>)
+    ?.concat((chain.evms as Array<any>) || [])
+    .concat((chain.consensus as Array<any>) || []);
+};
+
 export {
   isServiceSupported,
   isServiceBeta,
@@ -194,5 +316,10 @@ export {
   isChainDeprecated,
   isChainConsensusLayer,
   findChainById,
+  findSubnetMainnet,
   hasChainFullBlockSupport,
+  getNumberOfSupportedChains,
+  getChainStatus,
+  getSupportedServices,
+  getChainSubnets,
 };
