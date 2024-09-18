a2259d1f0de9a14980491cc8bd998ffc3cc70814
Yevhen Haievyi <yevhen.haievyi@gmail.com>
2024-09-18 16:23:09 +0300

    updated excluded chains for tests

--- scripts/known-issues-chains.js
+++ scripts/known-issues-chains.js
@@ -1,7 +1,18 @@
 const affectedChains = [
   {
     id: 'cosmoshub',
-    affected_services: ['firehose'],
+    affected_services: ['firehose', 'substreams'],
+    standard: 'cosmos',
+  },
+  {
+    id: 'theta',
+    affected_services: ['firehose', 'substreams'],
+    standard: 'cosmos',
+  },
+  {
+    id: 'osmosis',
+    affected_services: ['firehose', 'substreams'],
+    standard: 'cosmos',
   },
 ];
 
