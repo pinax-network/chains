73e234af074bd81cd16edb2aa0bf9078ad5a6b49
Yaro Shkvorets <shkvorets@gmail.com>
2025-11-18 12:58:44 -0500

    Deprecate boba and xai (#45)
    * remove boba and xai
    
    * bump version

--- data/chains/V2/xai/meta.ts
+++ data/chains/V2/xai/meta.ts
@@ -21,17 +21,17 @@ const meta: ___InternalChain = {
     rpc: {
       beta_released_at: '2024-08-01T00:00:00.000Z',
       full_released_at: null,
-      deprecated_at: null,
+      deprecated_at: '2025-11-17T00:00:00.000Z',
     },
     firehose: {
       beta_released_at: '2024-08-01T00:00:00.000Z',
       full_released_at: null,
-      deprecated_at: null,
+      deprecated_at: '2025-11-17T00:00:00.000Z',
     },
     substreams: {
       beta_released_at: '2024-08-01T00:00:00.000Z',
       full_released_at: null,
-      deprecated_at: null,
+      deprecated_at: '2025-11-17T00:00:00.000Z',
     },
     datasets: {
       beta_released_at: null,
